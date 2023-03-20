d28cb9e39243850097d2870d788fe5ff19fbc9cf
Orelba <orel450@gmail.com>
2023-03-20 13:29:21 +0200

    Arrange factory functions in a more readable way

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -2,61 +2,75 @@ const Gameboard = () => {
   // .map is used here to pass new instances of the object (.fill passes references)
   const board = new Array(100).fill().map(() => ({ hasShip: false, isShot: false }))
 
+  const createLocationArray = (ship, location, axis) => {
+    const locationArray = []
+    for (let i = 0; i < ship.length; i += 1) {
+      if (axis === 'x') {
+        locationArray.push(location + i)
+      } else {
+        locationArray.push(location + i * 10)
+      }
+    }
+    return locationArray
+  }
+
+  const checkCollisions = (shipLocationArray) => {
+    const collisions = [9, 19, 29, 39, 49, 59, 69, 79, 89]
+    // Check if ship placement exceeds board boundries (y axis collisions)
+    if (shipLocationArray.some((loc) => !board[loc])) return false
+
+    // Check collisions with other ships
+    if (shipLocationArray.some((loc) => board[loc].hasShip)) return false
+
+    // Check collisions with the x axis
+    // eslint-disable-next-line max-len
+    if (collisions.some((n) => [n, n + 1].every((indexes) => shipLocationArray.includes(indexes)))) return false
+    return true
+  }
+
+  const placeShip = (ship, location, axis = 'x') => {
+    const locationArray = createLocationArray(ship, location, axis)
+    if (!checkCollisions(locationArray)) {
+      const msg = `Placement of ship of length ${ship.length} at index ${location} on axis ${axis.toUpperCase()} is not valid.`
+      throw new Error(msg)
+    }
+
+    locationArray.forEach((loc) => {
+      board[loc].hasShip = ship
+    })
+    return true
+  }
+
+  const checkIfShotHit = (location) => !!(board[location].hasShip)
+
+  const receiveAttack = (location) => {
+    if (checkIfShotHit(location)) board[location].hasShip.hit()
+    board[location].isShot = true
+  }
+
+  const isGameOver = () => {
+    const shipSet = new Set()
+    let sunkShipsCounter = 0
+
+    board.forEach((cell) => {
+      if (cell.hasShip) shipSet.add(cell.hasShip)
+    })
+
+    shipSet.forEach((ship) => {
+      if (ship.isSunk()) sunkShipsCounter += 1
+    })
+
+    return shipSet.size === sunkShipsCounter
+  }
+
   return {
     board,
-    createLocationArray(ship, location, axis) {
-      const locationArray = []
-      for (let i = 0; i < ship.length; i += 1) {
-        if (axis === 'x') {
-          locationArray.push(location + i)
-        } else {
-          locationArray.push(location + i * 10)
-        }
-      }
-      return locationArray
-    },
-    checkCollisions(shipLocationArray) {
-      const collisions = [9, 19, 29, 39, 49, 59, 69, 79, 89]
-      // Check if ship placement exceeds board boundries (y axis collisions)
-      if (shipLocationArray.some((loc) => !this.board[loc])) return false
-
-      // Check collisions with other ships
-      if (shipLocationArray.some((loc) => this.board[loc].hasShip)) return false
-
-      // Check collisions with the x axis
-      // eslint-disable-next-line max-len
-      if (collisions.some((n) => [n, n + 1].every((indexes) => shipLocationArray.includes(indexes)))) return false
-      return true
-    },
-    placeShip(ship, location, axis = 'x') {
-      const locationArray = this.createLocationArray(ship, location, axis)
-      if (!this.checkCollisions(locationArray)) return
-
-      locationArray.forEach((loc) => {
-        this.board[loc].hasShip = ship
-      })
-    },
-    checkIfShotHit(location) {
-      return !!(this.board[location].hasShip)
-    },
-    receiveAttack(location) {
-      if (this.checkIfShotHit(location)) this.board[location].hasShip.hit()
-      this.board[location].isShot = true
-    },
-    isGameOver() {
-      const shipSet = new Set()
-      let sunkShipsCounter = 0
-
-      this.board.forEach((cell) => {
-        if (cell.hasShip) shipSet.add(cell.hasShip)
-      })
-
-      shipSet.forEach((ship) => {
-        if (ship.isSunk()) sunkShipsCounter += 1
-      })
-
-      return shipSet.size === sunkShipsCounter
-    },
+    createLocationArray,
+    checkCollisions,
+    placeShip,
+    checkIfShotHit,
+    receiveAttack,
+    isGameOver,
   }
 }
 
--- src/modules/player.js
+++ src/modules/player.js
@@ -1,24 +1,28 @@
 const Player = (name) => {
   const generateRandomLocFromArray = (array) => array[Math.floor(Math.random() * array.length)]
 
+  const attack = (location, gameboard) => {
+    if (gameboard.board[location].isShot) return false
+    gameboard.receiveAttack(location)
+    return true
+  }
+
+  const makeRandomAttack = (gameboard) => {
+    // Make array of valid board indexes with isShot === false
+    const validCellsArr = gameboard.board.reduce((acc, curr, idx) => {
+      if (!curr.isShot) acc.push(idx)
+      return acc
+    }, [])
+    // Choose a random location from the valid indexes array
+    const randomLoc = generateRandomLocFromArray(validCellsArr)
+
+    return attack(randomLoc, gameboard)
+  }
+
   return {
     name,
-    attack(location, gameboard) {
-      if (gameboard.board[location].isShot) return false
-      gameboard.receiveAttack(location)
-      return true
-    },
-    makeRandomAttack(gameboard) {
-      // Make array of valid board indexes with isShot === false
-      const validCellsArr = gameboard.board.reduce((acc, curr, idx) => {
-        if (!curr.isShot) acc.push(idx)
-        return acc
-      }, [])
-      // Choose a random location from the valid indexes array
-      const randomLoc = generateRandomLocFromArray(validCellsArr)
-
-      return this.attack(randomLoc, gameboard)
-    },
+    attack,
+    makeRandomAttack,
   }
 }
 
